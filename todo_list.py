9d2ea95aae0ce218bd67329c4edf03b613d1b4c2
Partha Sarati Das <pd818616@gmail.com>
2025-02-24 16:30:48 +0530

    Add files via upload

---
+++ todo_list.py
@@ -0,0 +1,20 @@
+list = []
+
+while True:
+    enter_task = input("Enter your task, or quit")
+    if (enter_task == "quit") :
+        break
+    list.append(enter_task)
+for lists in list :
+    enumerate(list)
+    print(lists)
+
+
+    
+    
+    
+
+
+
+
+
